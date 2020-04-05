3195771eefca1ec659b352549fee5cf82fddd97f
wafuwafu13 <mariobaske@i.softbank.jp>
2020-04-05 11:43:18 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content>
+      <Base/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from 'vue';
+import Base from './components/Base.vue';
 
-#nav {
-  padding: 30px;
+export default Vue.extend({
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  components: {
+    Base,
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    //
+  }),
+});
+</script>
---
+++ src/components/Base.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <p><router-link to="/">home</router-link></p>
+    <p><router-link to="/about">about</router-link></p>
+    <router-view></router-view>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'Base'
+  })
+</script>
